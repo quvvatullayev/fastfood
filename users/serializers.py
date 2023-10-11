dbd0da52c32a162ef4cb48956d1505b4c0e488cb
ogabek <quvvatullayev@gmail.com>
2023-10-11 14:33:34 +0500

    order serializer is created

--- users/serializers.py
+++ users/serializers.py
@@ -10,3 +10,8 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'

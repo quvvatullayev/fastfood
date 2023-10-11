27d6ef550a014d4ca2f03a943546d3c7378e9414
ogabek <quvvatullayev@gmail.com>
2023-10-11 13:55:08 +0500

    category is created

--- product/models.py
+++ product/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+    img = models.ImageField(upload_to='../categoryimgs')
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ["-id"]
+        

26e79c3b17d50c8886269c1eb5781232d923c9f1
ogabek <quvvatullayev@gmail.com>
2023-10-11 14:20:34 +0500

    user model is created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserModel(User):
+    phonenumber = models.CharField(max_length=20)
+    address = models.CharField(max_length=50)
+    telegramid = models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.username
+    

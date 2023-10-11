84d40270207d354e83f9d10476fa15a2154c7865
ogabek <quvvatullayev@gmail.com>
2023-10-11 14:58:00 +0500

    app models are add admin viwe

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserModel, Cart, Order
+from django.contrib.auth.models import Group
 
 # Register your models here.
+admin.site.register([UserModel, Cart, Order])
+admin.site.unregister([Group])

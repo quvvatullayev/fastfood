31f4bdf9fc1c54c30b3e8d2085ab293b3ca9eac8
ogabek <quvvatullayev@gmail.com>
2023-10-11 15:48:53 +0500

    fix

--- users/views/order.py
+++ users/views/order.py
@@ -2,11 +2,22 @@ from rest_framework import generics
 from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
-from ..serializers import Order, OrderSerializer
+from ..serializers import Order, OrderSerializer, UserModel, UserSerializer
 
 class OrdersView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+    def list(self, request:Request, *args, **kwargs):
+        telegramid = kwargs['telegramid']
+        user_obj = UserModel.objects.get(telegramid = telegramid)
+
+        orders_obj = Order.objects.filter(user = user_obj)
+        orders = OrderSerializer(orders_obj, many = True)
+
+        return Response({
+            'orders':orders.data
+        })
 
 class OrderView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()

176c7d2aea60b95c39cf113e3a35939cdc4257e3
ogabek <quvvatullayev@gmail.com>
2023-10-11 14:31:41 +0500

    cart serializer is created

--- users/serializers.py
+++ users/serializers.py
@@ -2,7 +2,11 @@ from rest_framework import serializers
 from .models import UserModel, Cart, Order
 
 class UserSerializer(serializers.ModelSerializer):
-    class Model:
+    class Meta:
         model = UserModel
         fields = ['id', 'username', 'phonenumber', 'address', 'telegramid']
 
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
